139296ef1e49067e78a2d04362a3188587ba2258
Texhnolyze <ichigobankai136@gmail.com>
2022-09-04 19:47:03 -0500

    comentarios y opciones salir

--- src/com/example/array/Ejercicio3.java
+++ src/com/example/array/Ejercicio3.java
@@ -11,6 +11,10 @@ public class Ejercicio3 {
     private static final String[] ciudad = new String[proveedor.length];
     private static final String[] numeroArticulo = new String[proveedor.length];
 
+    private static int numeroArticuloInt;
+
+
+
     public static void main(String[] args) {
         int op;
 
@@ -23,6 +27,7 @@ public class Ejercicio3 {
             System.out.println("3. Cambiar ubicación del proveedor");
             System.out.println("4. Aumentar o disminuir del proveedor");
             System.out.println("5. Mostrar info de proveedores");
+            System.out.println("6. Salir");
 
             op = sc.nextInt();
 
@@ -65,10 +70,15 @@ public class Ejercicio3 {
 
         System.out.println("Coloque el nombre a buscar");
         String nombreProveedor = sc.nextLine();
-
+        // comprueba dentro del array, este el nombre del proveedor
+        // lo convertimos en una lista para poder utilizar el metodo contains
+        // y encontrar el nombre que colocamos en la variables
         if (Arrays.asList(proveedor).contains(nombreProveedor)) {
-            System.out.println(nombreProveedor);
 
+            System.out.println(nombreProveedor);
+            // aunque la informacion este dentro de diferentes arrays, la informacion
+            // comparte la misma posicion por eso solo es necesario conocer la posicion
+            // del nombre pora poder sacar la demás informacion
             int index = Arrays.asList(proveedor).indexOf(nombreProveedor);
             System.out.println("Esta en la posicion " + index);
 
@@ -86,18 +96,21 @@ public class Ejercicio3 {
 
         System.out.println("Coloque el nombre a buscar");
         String nombreProveedor = sc.nextLine();
-
+        // de la misma forma vamos a usar el nombre para comprabar que existe el proveedor
         if (Arrays.asList(proveedor).contains(nombreProveedor)) {
-
+            // conseguimo el index
             int index = Arrays.asList(proveedor).indexOf(nombreProveedor);
 
             System.out.println(ciudad[index]);
 
+            // preguntamos por la nueva ciudad
             System.out.println("Nueva ciudad");
             String nombreNuevaCiudad = sc.nextLine();
 
+            // sobre escribimos la informacion con el metodo replace
             ciudad[index] = ciudad[index].replace(ciudad[index], nombreNuevaCiudad);
 
+            //imprimos la info con los cambios hechos
             System.out.println("Nombre " + proveedor[index]);
             System.out.println("Ciudad " + ciudad[index]);
             System.out.println("Articulos " + numeroArticulo[index]);
@@ -109,8 +122,10 @@ public class Ejercicio3 {
 
     public static void aumentarDisminuirArticulos() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("¿Que aumentar o disminuir numero de productos?");
 
+        System.out.println("¿Que aumentar o disminuir numero de productos?");
+        // forma similar a la utlizar en otros metodos sacamos la posicion de la info
+        // del proveedor atraves del nombre del proveedor
         System.out.println("Escribe el nombre del proveedor");
         String nombre = sc.nextLine();
 
@@ -121,23 +136,33 @@ public class Ejercicio3 {
         int op = sc.nextInt();
         switch (op) {
             case 1:
-                int numeroProductos = Integer.parseInt(numeroArticulo[index]);
-                numeroProductos++;
+                // convertimos el numero de articulos de string a int
+                // int numeroProductos = Integer.parseInt(numeroArticulo[index]);
+                numeroArticuloInt = Integer.parseInt(numeroArticulo[index]);
+                //aumentamos su valor por uno
+                numeroArticuloInt++;
+                //imprimos info
                 System.out.println("Informacion del proveedor ");
                 System.out.println("Nombre " + proveedor[index]);
-                System.out.println("Numero de productos " + numeroProductos);
+                System.out.println("Numero de productos " + numeroArticuloInt);
                 break;
             case 2:
-                int numeroProductosD = Integer.parseInt(numeroArticulo[index]);
-                numeroProductosD--;
+                // convertimos el numero de articulos de string a int
+                numeroArticuloInt = Integer.parseInt(numeroArticulo[index]);
+                // restamos su valor por uno
+                numeroArticuloInt--;
+                //imprimos info
                 System.out.println("Informacion del proveedor ");
                 System.out.println("Nombre " + proveedor[index]);
-                System.out.println("Numero de productos " + numeroProductosD);
+                System.out.println("Numero de productos " + numeroArticuloInt);
                 break;
 
         }
     }
 
+    /**
+     * Metodo para imprimir la info de todos los proveedores
+     */
     public static void info() {
         System.out.println("INFO");
         for (int i = 0; i < proveedor.length; i++) {
@@ -146,7 +171,9 @@ public class Ejercicio3 {
             System.out.println(numeroArticulo[i]);
         }
     }
-
+    /**
+     * Metodo para agregar dos proveedores
+     */
 
     public static void agregarProveedores() {
         Scanner sc = new Scanner(System.in);
