7811d41193fb14c55513a957386145dcbc87712a
Texhnolyze <ichigobankai136@gmail.com>
2022-09-06 17:28:37 -0500

    add: comentarios de la matriz 1

--- src/com/example/matrices/Matrices1.java
+++ src/com/example/matrices/Matrices1.java
@@ -8,11 +8,13 @@ public class Matrices1 {
 
         Scanner sc = new Scanner(System.in);
         final int FILAS = 2, COLUMNAS = 2;
-
+        // matriz 1 para la suma
         int [][] matrices = new int[FILAS][COLUMNAS];
+
+        // matriz 2 para la suma
         int [][] matrices2 = new int[FILAS][COLUMNAS];
 
-
+        //Matriz donde guarda la suma de las dos primeras matrices
         int [][] matricesResultado = new int[FILAS][COLUMNAS];
 
         //Se introducen por teclado los valores de la matriz
@@ -27,18 +29,20 @@ public class Matrices1 {
             }
         }
 
-
+        // se suma y muestra el resultado en otra matriz
         for (int i = 0; i < FILAS; i++) {
             for (int j = 0; j < COLUMNAS; j++) {
-
+                // se guarda un valor de las matrices en otra variable temporal
                 int matriz1 = matrices[i][j];
                 int matriz2 = matrices2[i][j];
 
+                // se hace la suma y se guarda el valor
                 int suma = matriz1 + matriz2;
 
+                // el valor guarda en la posición que le corresponde
                 matricesResultado [i][j] = suma;
 
-
+                // se imprime la matriz resultado
                 System.out.println("Resultado " + matricesResultado[i][j]);
             }
         }
