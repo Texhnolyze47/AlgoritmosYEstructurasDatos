f27b19e2101e38f5f9b5fe2a3d395ebc858b9cfb
josafat <josa221001@gmail.com>
2022-09-20 18:58:09 -0500

    add: agregando orden de mayor a menor

--- src/com/example/ordenamiento/Shell.java
+++ src/com/example/ordenamiento/Shell.java
@@ -82,7 +82,24 @@ public class Shell{
      * Metodo que usa el algoritmo de seleccion para ordenar de mayor a menor
      */
     public static void seleccionMayorMenor(){
-        // Falta esta parte xd
+        int salto, aux, i;
+        boolean cambios;
+
+        for (salto = array.length / 2; salto != 0; salto /= 2) {
+            cambios = true;
+            while (cambios) {   // Mientras se intercambie algún elemento
+                cambios = false;
+                for (i = salto; i < array.length; i++)   // se da una pasada
+                {
+                    if (array[i - salto] < array[i]) {       // y si están desordenados
+                        aux = array[i];                  // se reordenan
+                        array[i] = array[i - salto];
+                        array[i - salto] = aux;
+                        cambios = true;              // y se marca como cambio.
+                    }
+                }
+            }
+        }
     }
 
     /**
