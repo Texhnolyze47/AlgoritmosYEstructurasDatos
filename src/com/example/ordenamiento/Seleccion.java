add3583aaeaad84a7734ae26a357011a31fff412
Texhnolyze <ichigobankai136@gmail.com>
2022-09-19 20:18:07 -0500

    add: menu y opcion de menor a mayor y mayor a menor

--- src/com/example/ordenamiento/Seleccion.java
+++ src/com/example/ordenamiento/Seleccion.java
@@ -1,7 +1,5 @@
 package com.example.ordenamiento;
 
-import com.example.array.Array;
-
 import java.util.Scanner;
 
 public class Seleccion {
@@ -11,9 +9,27 @@ public class Seleccion {
     private static int[] array = new int[size];
 
     public static void main(String[] args) {
-        agregar();
-        seleccion();
-        imprimir();
+
+        int op;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Agregar");
+            System.out.println("2. Menor a mayor");
+            System.out.println("3. Mayor a menor");
+            System.out.println("4. imprimir");
+            System.out.println("5. Salir");
+            op = sc.nextInt();
+            switch (op) {
+                case 1 ->  agregar();
+                case 2 ->  seleccionMenorMayor();
+                case 3 ->  seleccionMayorMenor();
+                case 4 ->  imprimir();
+
+            }
+        }while (!((op) ==  5));
+        System.out.println("Adios");
+
     }
 
     public static  void agregar(){
@@ -28,7 +44,7 @@ public class Seleccion {
         }
     }
 
-    public static void seleccion(){
+    public static void seleccionMenorMayor(){
     int i,Menor = 0, k = 0, j;
         for (i = 0; i < array.length - 1 ; i++) {
             Menor = array[i];
@@ -43,6 +59,24 @@ public class Seleccion {
             array[i] = Menor;
         }
     }
+
+
+    public static void seleccionMayorMenor(){
+        int i,Menor = 0, k = 0, j;
+        for (i = 0; i < array.length - 1 ; i++) {
+            Menor = array[i];
+            k = i;
+            for (j = i+1; j < array.length;j++){
+                if (array[j] > Menor){
+                    Menor = array[j];
+                    k = j;
+                }
+            }
+            array[k] = array[i];
+            array[i] = Menor;
+        }
+    }
+
 
     public static void imprimir(){
         System.out.println("Arreglo ordenado");
