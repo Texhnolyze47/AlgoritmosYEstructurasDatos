0f578f99409bdb4ca9310dcfe073f09bc673e136
Texhnolyze47 <ichigobankai136@gmail.com>
2022-11-08 12:14:01 -0600

    cambio de nombre de la función

--- src/com/example/recursividad/ejercicios2/BinarioDecimal.java
+++ src/com/example/recursividad/ejercicios2/BinarioDecimal.java
@@ -3,16 +3,16 @@ package com.example.recursividad.ejercicios2;
 public class BinarioDecimal {
     public static void main(String[] args) {
         String numeroBinario = "101101";
-        System.out.println(recursive(numeroBinario));
+        System.out.println(binarioEntero(numeroBinario));
     }
 
-    public static int recursive(String binaryNumber) {
+    public static int binarioEntero(String binaryNumber) {
         int decimal = 0;
         int length = binaryNumber.length();
         if (length > 0) {
             String substring = binaryNumber.substring(1);
             int digit = Character.getNumericValue(binaryNumber.charAt(0));
-            decimal = digit * (int) Math.pow(2, length - 1) + recursive(substring);
+            decimal = digit * (int) Math.pow(2, length - 1) + binarioEntero(substring);
         }
         return decimal;
     }//output: decimal = 10
